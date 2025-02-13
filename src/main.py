1a53a5a62b441709daa64d26308b6367005de610
Anto <antonio.jeleriu@student.unitbv.ro>
2025-02-13 09:25:34 -0800

    feat: use a population

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import pygame
 import config
 import pipe
 from src.ground import Ground
-from src.player import Player
+from src.population import Population
 
 pygame.init()
 window = pygame.display.set_mode((config.SCREEN_WIDTH, config.SCREEN_HEIGHT))
@@ -19,14 +19,15 @@ def spawn_pipe(pipes):
     pipes.appendleft(pipe.Pipe())
 
 def main():
+    population_size = 100
+
     pipe_spawn_time = 0
     ground = Ground()
     pipes = collections.deque()
-    player = Player()
+    population = Population(population_size)
 
     while True:
         poll_events()
-
         window.fill("purple")
 
         ground.draw(window)
@@ -37,13 +38,13 @@ def main():
 
         for pipe in list(pipes):
             pipe.draw(window)
-            pipe.update()
+            pipe.update(population.players[0])
 
             if not pipe.is_active():
                 pipes.pop()
 
-        player.update(pipes, ground)
-        player.draw(window)
+        population.update_players(pipes, ground)
+        population.draw(window, pipes)
 
         pygame.display.flip()
         pipe_spawn_time -= 1
