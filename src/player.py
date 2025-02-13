d8b4103175982453fa26dc9e9ec61409168006a2
Anto <antonio.jeleriu@student.unitbv.ro>
2025-02-13 09:26:50 -0800

    feat: the perceptron thinks about the next move

--- src/player.py
+++ src/player.py
@@ -1,6 +1,10 @@
+import numpy as np
 import pygame
+from pygame.display import update
+
 import config
 import random
+import brain
 
 class Player:
     def __init__(self):
@@ -15,9 +19,24 @@ class Player:
             config.PLAYER_SIZE
             )
 
-    def draw(self, window):
+        self.brain = brain.Brain()
+        self.vision = np.array([0.5, 1.0, 0.5])
+
+    def draw(self, window, pipes):
         if self.active:
             pygame.draw.rect(window, self.color, self.rectangle)
+
+            closest = pipes[0]
+            for p in pipes:
+                if not p.passed:
+                    closest = p
+
+            pygame.draw.line(window, "white", self.rectangle.center,
+                             (self.rectangle.center[0], closest.top_pipe.bottom))
+            pygame.draw.line(window, "white", self.rectangle.center,
+                             (closest.top_pipe.x, self.rectangle.center[1]))
+            pygame.draw.line(window, "white", self.rectangle.center,
+                             (self.rectangle.center[0], closest.bottom_pipe.y))
 
     def update(self, pipes, ground):
         if self.pipe_collision(pipes) or self.bounds_collision(ground):
@@ -26,19 +45,32 @@ class Player:
         if self.active:
             self.rectangle.y += self.velocity
             self.velocity = min(self.velocity + config.VELOCITY_RATE, config.MAX_VELOCITY)
+            self.update_vision(pipes)
 
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_SPACE]:
-                self.flap()
+            self.think()
+
+    def update_vision(self, pipes):
+        closest = pipes[0]
+        for p in pipes:
+            if not p.passed:
+                closest = p
+
+        self.vision[0] = max(0, self.rectangle.center[1] - closest.top_pipe.bottom) / 50
+        self.vision[1] = max(0, self.rectangle.center[0] - closest.top_pipe.x) / 50
+        self.vision[2] = max(0, self.rectangle.center[1] - closest.bottom_pipe.y) / 50
+
+    def think(self):
+        flap_chance = self.brain.make_prediction(self.vision)
+        if flap_chance > 0.8:
+            self.flap()
 
     def flap(self):
         if not self.flapping:
             self.velocity = -config.MAX_VELOCITY
             self.flapping = True
 
-        if self.velocity >= 3:
+        if self.velocity >= 1:
             self.flapping = False
-
 
     def pipe_collision(self, pipes):
         for pipe in pipes:
