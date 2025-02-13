a9c839972c2c34cc179f81bfd929421704caf744
Anto <antonio.jeleriu@student.unitbv.ro>
2025-02-13 09:24:57 -0800

    add: added numpy as a requirement

---
+++ src/brain.py
@@ -0,0 +1,13 @@
+import random
+
+import numpy as np
+
+class Brain:
+    def __init__(self):
+        self.weights = np.array([random.uniform(-1, 1), random.uniform(-1, 1), random.uniform(-1, 1)])
+
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def make_prediction(self, vision):
+        return self._sigmoid(np.dot(vision, self.weights))
