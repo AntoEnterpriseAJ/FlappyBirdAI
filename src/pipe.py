9de1b2c4b50fffafe03d0060c2a4f76edbfbd9b0
Anto <antonio.jeleriu@student.unitbv.ro>
2025-02-17 13:56:27 +0200

    feat: new pipe texture

--- src/pipe.py
+++ src/pipe.py
@@ -8,6 +8,8 @@ class Pipe:
         self.active = True
         self.passed = False
         self.x = config.SCREEN_WIDTH
+        self.texture_top = pygame.image.load('img/pipe.png')
+        self.texture_bottom = pygame.image.load('img/pipe.png')
 
         gap_y = random.uniform(config.PIPE_GAP_SIZE, config.GROUND_Y - config.PIPE_GAP_SIZE * 2)
 
@@ -16,19 +18,35 @@ class Pipe:
             self.x,
             gap_y + config.PIPE_GAP_SIZE,
             config.PIPE_WIDTH,
-            config.GROUND_Y - (gap_y + config.PIPE_GAP_SIZE)
+            config.GROUND_Y - (gap_y + config.PIPE_GAP_SIZE - 1)
+        )
+
+        self.texture_top = pygame.transform.scale(
+            self.texture_top,(config.PIPE_WIDTH, self.texture_top.get_height())
+        )
+        self.texture_top = pygame.transform.rotate(self.texture_top, 180)
+
+        self.texture_bottom = pygame.transform.scale(
+            self.texture_bottom, (config.PIPE_WIDTH, self.texture_bottom.get_height())
         )
 
     def draw(self, window):
         if not self.active:
-            pass
+            return
 
-        pygame.draw.rect(window, "red", self.top_pipe)
-        pygame.draw.rect(window, "red", self.bottom_pipe)
+        top_pipe_coords = (self.top_pipe.x,
+                           self.top_pipe.y - (self.texture_top.get_size()[1] - self.top_pipe.height))
+
+        window.blit(self.texture_top, top_pipe_coords)
+        window.blit(
+            self.texture_bottom,
+            (self.bottom_pipe.x, self.bottom_pipe.y),
+            (0, 0, self.bottom_pipe.width, self.bottom_pipe.height)
+        )
 
     def update(self, player):
         if not self.active:
-            pass
+            return
 
         self.x -= 1.0
         self.top_pipe.x = self.x
